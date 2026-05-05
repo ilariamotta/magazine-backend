2a71fa5cc00f3af0435725dd38b4231cbdbde7bf
ilariamotta <ilaria.motta93@gmail.com>
2026-05-05 18:32:19 +0200

    feat: index authors

--- app/Http/Controllers/Admin/AuthorController.php
+++ app/Http/Controllers/Admin/AuthorController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Author::all();
+        return view('admin.authors.index', compact('authors'));
+    }
+
+        public function show(Author $author)
+        {
+            return view('admin.authors.show', compact('author'));
+        }
 }
---
+++ resources/views/admin/authors/index.blade.php
@@ -0,0 +1,130 @@
+@extends('admin.layouts.master')
+
+@section('title', 'Autori - Pixel Pop Admin')
+
+@section('dashboard')
+
+    {{-- HEADER PAGINA --}}
+    <div class="mb-4">
+        <h1 class="fw-bold mb-1">Autori</h1>
+        <p class="text-muted mb-0">
+            Tutti gli autori di Pixel Pop.
+        </p>
+    </div>
+
+    {{-- AZIONI RAPIDE --}}
+    <div class="card admin-card border-0 shadow-sm rounded-4 mb-4">
+        <div class="card-body p-4">
+            <h5 class="fw-bold mb-3">Azioni rapide</h5>
+
+            <div class="d-flex flex-column flex-sm-row gap-3">
+                <a href="{{ route('admin.articles.create') }}" class="btn btn-outline-pixel px-4 py-2">
+                    + Aggiungi nuovo articolo
+                </a>
+
+                <button type="button" class="btn btn-outline-dark px-4 py-2">
+                    + Aggiungi categoria
+                </button>
+
+                <button type="button" class="btn btn-outline-dark px-4 py-2">
+                    + Aggiungi autore
+                </button>
+            </div>
+        </div>
+    </div>
+
+    {{-- LISTA ARTICOLI --}}
+    <div class="card admin-card border-0 shadow-sm rounded-4 mb-4">
+        <div class="card-header bg-white border-0 pt-4 px-4">
+            <h5 class="fw-bold mb-1">Autori</h5>
+            <p class="text-muted mb-0">
+                Elenco degli autori del magazine.
+            </p>
+        </div>
+
+        <div class="card-body px-4">
+            <div class="table-responsive">
+                <table class="table align-middle mb-0">
+                    <thead>
+                        <tr class="text-muted">
+                            <th>Avatar</th>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th>Azioni</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach($authors as $author)
+                            <tr>
+                                <td>
+                                    @if ($author->avatar_image)
+                                    <div class="rounded-circle overflow-hidden d-inline-block" style="width: 40px; height: 40px;">
+                                        {{ $author->avatar_image }}
+                                    </div>
+                                    @else 
+                                    <img src="https://t3.ftcdn.net/jpg/06/03/30/74/360_F_603307418_jya3zntHWjXWn3WHn7FOpjFevXwnVP52.jpg" alt="Avatar" class="rounded-circle" style="width: 50px; height: 50px;">
+                                    @endif
+                                </td>
+                                <td>
+                                        <small class="text-muted">
+                                            {{ $author->name }}
+                                        </small>
+
+                                </td>
+
+                                <td>
+                                    <small class="text-muted">
+                                        {{ $author->email }}
+                                    </small>
+                                </td>
+
+                                <td>
+
+                                {{-- <a href="{{ route('admin.articles.show', $article) }}" class="btn btn-outline-pixel btn-sm mb-1">
+                                    Vedi</a>
+                           
+            <a href="{{ route('admin.articles.edit', $article) }}" class="btn btn-outline-pixel btn-sm mb-1">
+                Modifica</a>
+            <button type="button" class="btn btn-outline-dark mb-1 btn-sm" data-bs-toggle="modal" data-bs-target="#deleteArticleModal">
+                Elimina</button> --}}
+
+        </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+{{-- modale --}}
+{{-- <div class="modal fade" id="deleteArticleModal" tabindex="-1" aria-labelledby="deleteArticleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content rounded-4 border-0 shadow">
+            <div class="modal-header">
+                <h5 class="modal-title fw-bold" id="deleteArticleModalLabel">
+                    Sei sicuro di voler eliminare l'articolo?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Chiudi"></button>
+            </div>
+            <div class="modal-body">
+                <p class="mb-0">
+                    Stai eliminando l'articolo:
+                    <strong>{{ $article->title }}</strong>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-dark btn-sm" data-bs-dismiss="modal">
+                    Annulla
+                </button>
+
+                <form action="{{ route('admin.articles.destroy', $article) }}" method="POST">
+                    @csrf
+                    @method("DELETE")
+                    <input type="submit" value="Elimina" class="btn btn-outline-pixel btn-sm">
+                </form>
+            </div>
+        </div>
+    </div>
+</div> --}}
+@endsection
